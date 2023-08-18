fb621e5a949100738787ee339bbc031181cd2ba8
Max Anurin <maxim.anurin@zxteam.org>
2023-08-18 11:37:33 +0300

    Init StorageProvider before endpoints

--- src/index.ts
+++ src/index.ts
@@ -44,10 +44,10 @@ export class RestartRequireException extends FLauncherRestartRequiredException {
 
 
 export default async function (executionContext: FExecutionContext, settings: Settings): Promise<FLauncherRuntime> {
-	executionContext = new FLoggerLabelsExecutionContext(executionContext, { 
+	executionContext = new FLoggerLabelsExecutionContext(executionContext, {
 		service: packageInfo.name,
 		version: packageInfo.version
-	 });
+	});
 
 	FDecimal.configure(new FDecimalBackendNumber(8, FDecimalRoundMode.Trunc));
 
@@ -70,214 +70,220 @@ export default async function (executionContext: FExecutionContext, settings: Se
 	}
 
 	const storageProvider: StorageProvider = Container.get(StorageProvider);
-
-	log.info(executionContext, "Initializing DI runtime...");
-	await FInitable.initAll(executionContext,
-		storageProvider,
-		ProviderLocator.default.get(MessageBusProvider),
-		ProviderLocator.default.get(ApiProvider),
-		ProviderLocator.default.get(EndpointsProvider),
-		ProviderLocator.default.get(HostingProvider)
-	);
+	await storageProvider.init(executionContext);
 
 	const itemsToDispose: Array<FDisposable> = [];
 	try {
 		{ // local scope
 			log.trace(executionContext, "Reading database versions...");
 			const versions: Array<string> = await storageProvider.databaseFactory
-				.using(executionContext, db =>  db.listVersions(executionContext));
+				.using(executionContext, db => db.listVersions(executionContext));
 			const versionChain: string = versions.join(" -> ");
 			log.info(executionContext, `Database versions chain: ${versionChain}`);
 		}
 
-		{
-			// Setup Management
-			const setupSettings: Settings.Setup | null = ProviderLocator.default.get(SettingsProvider).setup;
-			if (setupSettings !== null) {
-				const setupServiceProvider: SetupServiceProvider = ProviderLocator.default.get(SetupServiceProvider);
-				const wasChanged: boolean = await setupServiceProvider.setup(
-					executionContext = new FLoggerLabelsExecutionContext(executionContext, {
-						phase: "setup"
-					}),
-					setupSettings
-				);
 
-				if (wasChanged) {
-					throw new RestartRequireException({exitCode:100, message: "Setup process was applied. This action required restart of the service. Exiting..."});
-				}
-			} else {
-				log.info(executionContext, "Skip setup process due no setup settings");
-			}
-		}
+		log.info(executionContext, "Initializing DI runtime...");
+		await FInitable.initAll(executionContext,
+			ProviderLocator.default.get(MessageBusProvider),
+			ProviderLocator.default.get(ApiProvider),
+			ProviderLocator.default.get(EndpointsProvider),
+			ProviderLocator.default.get(HostingProvider)
+		);
 
-		/* ---------HARDCODED INITIALIZATION--------------- */
-		{
-			const hardcodedPublisherConfigurations: Array<{
-				readonly topicId: TopicIdentifier,
-				readonly topicName: string;
-				readonly topicDescription: string;
-				readonly topicMediaType: string;
-				// readonly ingressId: string;
-				// readonly publisherPath: string;
-				readonly ingressConfiguration: Settings.Setup.Ingress;
-			}> = [];
-			const hardcodedSubscriberConfigurations: Array<{
-				readonly topicIds: ReadonlyArray<string>;
-				readonly topicNames: ReadonlyArray<string>;
-				readonly egress: Settings.Setup.Egress;
-				// readonly deliveryHttpMethod: string;
-				// readonly deliveryUrl: URL;
-			}> = [];
-
-			const { setup } = settings;
-			if (setup !== null) {
-				const { ingresses, egresses, topics } = setup;
-
-				const topicsByIdMap = new Map<string, Settings.Setup.Topic>();
-				for (const topic of topics) {
-					topicsByIdMap.set(topic.topicId, topic);
-				}
-
-				for (const ingress of ingresses) {
-					hardcodedPublisherConfigurations.push({
-						topicId: TopicIdentifier.parse(topicsByIdMap.get(ingress.topicId)!.topicId),
-						topicName: topicsByIdMap.get(ingress.topicId)!.name,
-						topicDescription: topicsByIdMap.get(ingress.topicId)!.description,
-						topicMediaType: topicsByIdMap.get(ingress.topicId)!.mediaType,
-						ingressConfiguration: ingress
-					});
-				}
-
-				for (const egress of egresses) {
-					hardcodedSubscriberConfigurations.push({
-						topicIds: egress.sourceTopicIds,
-						topicNames: egress.sourceTopicIds.map(s => topicsByIdMap.get(s)!.name),
-						egress,
-						// deliveryHttpMethod: e.deliveryHttpMethod
-						// deliveryUrl: e.deliveryUrl,
-					});
-				}
-			}
-
-			const endpointsProvider: EndpointsProvider = ProviderLocator.default.get(EndpointsProvider);
-			const messageBusProvider: MessageBusProvider = ProviderLocator.default.get(MessageBusProvider);
-			const storageProvider: StorageProvider = ProviderLocator.default.get(StorageProvider);
-
-			// Setup HTTP ingress
-			for (const hardcodedPublisherConfiguration of hardcodedPublisherConfigurations) {
-				const ingressConfiguration = hardcodedPublisherConfiguration.ingressConfiguration;
-				const ingressId: IngressIdentifier = IngressIdentifier.parse(ingressConfiguration.ingressId);
-				if (ingressConfiguration.kind === Ingress.Kind.WebSocketClient) {
-					const webSocketClientIngress: WebSocketClientIngress = new WebSocketClientIngress(
-						{
-							topicId: hardcodedPublisherConfiguration.topicId,
-							topicName: hardcodedPublisherConfiguration.topicName,
-							topicDomain: null,
-							topicDescription: hardcodedPublisherConfiguration.topicDescription,
-							topicMediaType: hardcodedPublisherConfiguration.topicMediaType,
-						},
-						ingressId,
-						messageBusProvider.wrap,
-						{
-							wsOptions: {},
-							transformers: [],
-							url: ingressConfiguration.url
-						}
+		try {
+			{
+				// Setup Management
+				const setupSettings: Settings.Setup | null = ProviderLocator.default.get(SettingsProvider).setup;
+				if (setupSettings !== null) {
+					const setupServiceProvider: SetupServiceProvider = ProviderLocator.default.get(SetupServiceProvider);
+					const wasChanged: boolean = await setupServiceProvider.setup(
+						executionContext = new FLoggerLabelsExecutionContext(executionContext, {
+							phase: "setup"
+						}),
+						setupSettings
 					);
-					await webSocketClientIngress.init(executionContext);
-					itemsToDispose.push(webSocketClientIngress);
+
+					if (wasChanged) {
+						throw new RestartRequireException({ exitCode: 100, message: "Setup process was applied. This action required restart of the service. Exiting..." });
+					}
 				} else {
+					log.info(executionContext, "Skip setup process due no setup settings");
+				}
+			}
 
-					if (ingressConfiguration.kind !== Ingress.Kind.HttpHost) {
-						throw new FExceptionInvalidOperation(`Not supported yet: ${ingressConfiguration.kind}`);
+			/* ---------HARDCODED INITIALIZATION--------------- */
+			{
+				const hardcodedPublisherConfigurations: Array<{
+					readonly topicId: TopicIdentifier,
+					readonly topicName: string;
+					readonly topicDescription: string;
+					readonly topicMediaType: string;
+					// readonly ingressId: string;
+					// readonly publisherPath: string;
+					readonly ingressConfiguration: Settings.Setup.Ingress;
+				}> = [];
+				const hardcodedSubscriberConfigurations: Array<{
+					readonly topicIds: ReadonlyArray<string>;
+					readonly topicNames: ReadonlyArray<string>;
+					readonly egress: Settings.Setup.Egress;
+					// readonly deliveryHttpMethod: string;
+					// readonly deliveryUrl: URL;
+				}> = [];
+
+				const { setup } = settings;
+				if (setup !== null) {
+					const { ingresses, egresses, topics } = setup;
+
+					const topicsByIdMap = new Map<string, Settings.Setup.Topic>();
+					for (const topic of topics) {
+						topicsByIdMap.set(topic.topicId, topic);
 					}
-					const httpPublisherInstance: HttpHostIngress = new HttpHostIngress(
-						{
-							topicId: hardcodedPublisherConfiguration.topicId,
-							topicName: hardcodedPublisherConfiguration.topicName,
-							topicDomain: null,
-							topicDescription: hardcodedPublisherConfiguration.topicDescription,
-							topicMediaType: hardcodedPublisherConfiguration.topicMediaType,
-						},
-						ingressId,
-						messageBusProvider.wrap,
-						{
-							transformers: [],
-							bindPath: ingressConfiguration.path,
-							successResponseGenerator: () => ({
-								headers: ingressConfiguration.responseHeaders,
-								body: ingressConfiguration.responseBody,
-								statusCode: ingressConfiguration.responseStatusCode,
-								statusDescription: ingressConfiguration.responseStatusMessage,
-							})
+
+					for (const ingress of ingresses) {
+						hardcodedPublisherConfigurations.push({
+							topicId: TopicIdentifier.parse(topicsByIdMap.get(ingress.topicId)!.topicId),
+							topicName: topicsByIdMap.get(ingress.topicId)!.name,
+							topicDescription: topicsByIdMap.get(ingress.topicId)!.description,
+							topicMediaType: topicsByIdMap.get(ingress.topicId)!.mediaType,
+							ingressConfiguration: ingress
+						});
+					}
+
+					for (const egress of egresses) {
+						hardcodedSubscriberConfigurations.push({
+							topicIds: egress.sourceTopicIds,
+							topicNames: egress.sourceTopicIds.map(s => topicsByIdMap.get(s)!.name),
+							egress,
+							// deliveryHttpMethod: e.deliveryHttpMethod
+							// deliveryUrl: e.deliveryUrl,
+						});
+					}
+				}
+
+				const endpointsProvider: EndpointsProvider = ProviderLocator.default.get(EndpointsProvider);
+				const messageBusProvider: MessageBusProvider = ProviderLocator.default.get(MessageBusProvider);
+				const storageProvider: StorageProvider = ProviderLocator.default.get(StorageProvider);
+
+				// Setup HTTP ingress
+				for (const hardcodedPublisherConfiguration of hardcodedPublisherConfigurations) {
+					const ingressConfiguration = hardcodedPublisherConfiguration.ingressConfiguration;
+					const ingressId: IngressIdentifier = IngressIdentifier.parse(ingressConfiguration.ingressId);
+					if (ingressConfiguration.kind === Ingress.Kind.WebSocketClient) {
+						const webSocketClientIngress: WebSocketClientIngress = new WebSocketClientIngress(
+							{
+								topicId: hardcodedPublisherConfiguration.topicId,
+								topicName: hardcodedPublisherConfiguration.topicName,
+								topicDomain: null,
+								topicDescription: hardcodedPublisherConfiguration.topicDescription,
+								topicMediaType: hardcodedPublisherConfiguration.topicMediaType,
+							},
+							ingressId,
+							messageBusProvider.wrap,
+							{
+								wsOptions: {},
+								transformers: [],
+								url: ingressConfiguration.url
+							}
+						);
+						await webSocketClientIngress.init(executionContext);
+						itemsToDispose.push(webSocketClientIngress);
+					} else {
+
+						if (ingressConfiguration.kind !== Ingress.Kind.HttpHost) {
+							throw new FExceptionInvalidOperation(`Not supported yet: ${ingressConfiguration.kind}`);
 						}
-					);
-					//harcodedItemsToDispose.push(httpPublisherInstance);
-					for (const publisherApiRestEndpoint of endpointsProvider.ingressApiRestEndpoints) {
-						publisherApiRestEndpoint.addHttpPublisher(executionContext, httpPublisherInstance);
+						const httpPublisherInstance: HttpHostIngress = new HttpHostIngress(
+							{
+								topicId: hardcodedPublisherConfiguration.topicId,
+								topicName: hardcodedPublisherConfiguration.topicName,
+								topicDomain: null,
+								topicDescription: hardcodedPublisherConfiguration.topicDescription,
+								topicMediaType: hardcodedPublisherConfiguration.topicMediaType,
+							},
+							ingressId,
+							messageBusProvider.wrap,
+							{
+								transformers: [],
+								bindPath: ingressConfiguration.path,
+								successResponseGenerator: () => ({
+									headers: ingressConfiguration.responseHeaders,
+									body: ingressConfiguration.responseBody,
+									statusCode: ingressConfiguration.responseStatusCode,
+									statusDescription: ingressConfiguration.responseStatusMessage,
+								})
+							}
+						);
+						//harcodedItemsToDispose.push(httpPublisherInstance);
+						for (const publisherApiRestEndpoint of endpointsProvider.ingressApiRestEndpoints) {
+							publisherApiRestEndpoint.addHttpPublisher(executionContext, httpPublisherInstance);
+						}
+						await httpPublisherInstance.init(executionContext);
+						itemsToDispose.push(httpPublisherInstance);
 					}
-					await httpPublisherInstance.init(executionContext);
-					itemsToDispose.push(httpPublisherInstance);
+				}
+
+				// Setup egresses
+				for (const hardcodedSubscriberConfiguration of hardcodedSubscriberConfigurations) {
+					const egressId: EgressIdentifier = EgressIdentifier.parse(hardcodedSubscriberConfiguration.egress.egressId);
+					const channelFactories: Array<MessageBus.ChannelFactory> = [];
+					for (const topicIdStr of hardcodedSubscriberConfiguration.topicIds) {
+						const channelFactory = async (): Promise<MessageBus.Channel> => {
+							const topicId: TopicIdentifier = TopicIdentifier.parse(topicIdStr);
+							const channel = await messageBusProvider.wrap.retainChannel(executionContext, topicId, egressId);
+							return channel;
+						}
+						channelFactories.push(channelFactory);
+					}
+
+					for (const subscriberApiRestEndpoint of endpointsProvider.egressApiRestEndpoints) {
+						switch (hardcodedSubscriberConfiguration.egress.kind) {
+							case Egress.Kind.WebSocketHost:
+								const webSocketHostSubscriber = new WebSocketHostEgress(
+									{
+										baseBindPath: subscriberApiRestEndpoint.bindPath,
+										bindServers: subscriberApiRestEndpoint.servers,
+										egressId,
+										channelFactories
+									},
+								);
+								await webSocketHostSubscriber.init(executionContext);
+								itemsToDispose.push(webSocketHostSubscriber);
+								break;
+							case Egress.Kind.Webhook:
+								const httpClientSubscriber = new WebhookEgress(
+									{
+										deliveryHttpMethod: hardcodedSubscriberConfiguration.egress.method,
+										deliveryUrl: hardcodedSubscriberConfiguration.egress.url,
+										ssl: hardcodedSubscriberConfiguration.egress.ssl,
+										egressId,
+										channelFactories
+									}
+								);
+								await httpClientSubscriber.init(executionContext);
+								itemsToDispose.push(httpClientSubscriber);
+								break;
+							default:
+								throw new FExceptionArgument(`Unsupported egress type ${(hardcodedSubscriberConfiguration.egress as any).kind}`);
+						}
+					}
 				}
 			}
+		} catch (e) {
+			for (const hardcodedItem of itemsToDispose) { await hardcodedItem.dispose(); }
 
-			// Setup egresses
-			for (const hardcodedSubscriberConfiguration of hardcodedSubscriberConfigurations) {
-				const egressId: EgressIdentifier = EgressIdentifier.parse(hardcodedSubscriberConfiguration.egress.egressId);
-				const channelFactories: Array<MessageBus.ChannelFactory> = [];
-				for (const topicIdStr of hardcodedSubscriberConfiguration.topicIds) {
-					const channelFactory = async (): Promise<MessageBus.Channel> => {
-						const topicId: TopicIdentifier = TopicIdentifier.parse(topicIdStr);
-						const channel = await messageBusProvider.wrap.retainChannel(executionContext, topicId, egressId);
-						return channel;
-					}
-					channelFactories.push(channelFactory);
-				}
-
-				for (const subscriberApiRestEndpoint of endpointsProvider.egressApiRestEndpoints) {
-					switch (hardcodedSubscriberConfiguration.egress.kind) {
-						case Egress.Kind.WebSocketHost:
-							const webSocketHostSubscriber = new WebSocketHostEgress(
-								{
-									baseBindPath: subscriberApiRestEndpoint.bindPath,
-									bindServers: subscriberApiRestEndpoint.servers,
-									egressId,
-									channelFactories
-								},
-							);
-							await webSocketHostSubscriber.init(executionContext);
-							itemsToDispose.push(webSocketHostSubscriber);
-							break;
-						case Egress.Kind.Webhook:
-							const httpClientSubscriber = new WebhookEgress(
-								{
-									deliveryHttpMethod: hardcodedSubscriberConfiguration.egress.method,
-									deliveryUrl: hardcodedSubscriberConfiguration.egress.url,
-									ssl: hardcodedSubscriberConfiguration.egress.ssl,
-									egressId,
-									channelFactories
-								}
-							);
-							await httpClientSubscriber.init(executionContext);
-							itemsToDispose.push(httpClientSubscriber);
-							break;
-						default:
-							throw new FExceptionArgument(`Unsupported egress type ${(hardcodedSubscriberConfiguration.egress as any).kind}`);
-					}
-				}
-			}
+			await FDisposable.disposeAll(
+				// Endpoints should dispose first (reply 503, while finishing all active requests)
+				ProviderLocator.default.get(EndpointsProvider),
+				ProviderLocator.default.get(HostingProvider),
+				ProviderLocator.default.get(ApiProvider),
+				ProviderLocator.default.get(MessageBusProvider),
+				ProviderLocator.default.get(StorageProvider)
+			);
+			throw e;
 		}
 	} catch (e) {
-		for (const hardcodedItem of itemsToDispose) { await hardcodedItem.dispose(); }
-
-		await FDisposable.disposeAll(
-			// Endpoints should dispose first (reply 503, while finishing all active requests)
-			ProviderLocator.default.get(EndpointsProvider),
-			ProviderLocator.default.get(HostingProvider),
-			ProviderLocator.default.get(ApiProvider),
-			ProviderLocator.default.get(MessageBusProvider),
-			ProviderLocator.default.get(StorageProvider)
-		);
+		await storageProvider.dispose();
 		throw e;
 	}
 	/* ------------------------ */
@@ -295,7 +301,7 @@ export default async function (executionContext: FExecutionContext, settings: Se
 				ProviderLocator.default.get(EndpointsProvider),
 				ProviderLocator.default.get(HostingProvider),
 				ProviderLocator.default.get(MessageBusProvider),
-				ProviderLocator.default.get(StorageProvider)
+				storageProvider
 			);
 		}
 	};
