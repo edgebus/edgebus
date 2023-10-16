65180dc1e92f2ae180630366df3fe22e0dd9a907
Dmitry Detukov <dmitry.detukov@zxteam.net>
2023-10-16 15:48:55 +0300

    Refs #11 Implementation Web Admin

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,17 +3,6 @@
 
 set -e
 
-echo
-echo "Launch EdgeBus console..."
-echo
-
-# sed -i "s/background-color:#fff !important/background-color:${PGADMIN_BG_COLOR} !important/g" /pgadmin4/pgadmin/static/js/generated/pgadmin.css
-# if [ -z "${PGADMIN_BG_COLOR}" ]; then
-# 	echo "A variable PGADMIN_BG_COLOR is not set" >&2
-# 	exit 1
-# fi
-
-
 if [ "${1}" == "--use-ssl" ]; then
 	echo "Starting the NGINX(secured)..."
 	echo
